38cd408dde225a5bdf240d9bc2d03841ca2d6dfb
Boniface Mwenda <boniface.mwenda@andela.com>
2017-01-13 15:59:33 +0300

    Created settings to initialize application and db

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+import config
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object(config.config_environments['development'])
+
+db = SQLAlchemy(app)
