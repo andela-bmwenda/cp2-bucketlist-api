922fc0addbdff81bff34fdf51113afa59e46b919
Boniface Mwenda <boniface.mwenda@andela.com>
2017-01-27 00:08:38 +0300

    Configurations flask application and database

--- app/__init__.py
+++ app/__init__.py
@@ -4,5 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(config.config_environments['development'])
+app.config['SECRET_KEY'] = "123bucketlistapp"
 
 db = SQLAlchemy(app)
