24ea32f71faab214a6f350d2d48b2f5715d8262d
Boniface Mwenda <boniface.mwenda@andela.com>
2017-01-27 00:07:05 +0300

    Set urls for bucketlist items endpoints

--- manage.py
+++ manage.py
@@ -1,10 +1,9 @@
 from flask_restful import Api
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-from app import app
-from app import db
+from app import app, db
 from app.auth import Register, Login
-from app.bucketlist_api import BucketList, BucketListEntry
+from app.bucketlist_api import BucketLists, BucketListSingle
 from app.bucketlist_items import BucketListItems, BucketListItemSingle
 
 migrate = Migrate(app, db)
@@ -16,8 +15,8 @@ api = Api(app)
 
 api.add_resource(Register, '/auth/register')
 api.add_resource(Login, '/auth/login')
-api.add_resource(BucketList, '/bucketlists')
-api.add_resource(BucketListEntry, '/bucketlists/<int:bucketlist_id>')
+api.add_resource(BucketLists, '/bucketlists')
+api.add_resource(BucketListSingle, '/bucketlists/<int:bucketlist_id>')
 api.add_resource(BucketListItems, '/bucketlists/<int:bucketlist_id>/items')
 api.add_resource(BucketListItemSingle,
                  '/bucketlists/<int:bucketlist_id>/items/<int:item_id>')
