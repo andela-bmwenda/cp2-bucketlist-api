829957b266dbac8d0843164a26a3636ecffe50cb
Boniface Mwenda <boniface.mwenda@andela.com>
2017-01-27 00:20:24 +0300

    Added tests for registration and login

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -4,13 +4,7 @@ import json
 
 class AuthTestCase(BaseTestCase):
 
-    def setUp(self):
-
-        self.credentials = json.dumps({"username": "Hugo",
-                                      "password": 1234})
-
     def test_registration_with_invalid_format(self):
-
         data = json.dumps({"username": "Hugo",
                            "password": 1234,
                            "invalid": "Invalid entry"}
@@ -20,74 +14,85 @@ class AuthTestCase(BaseTestCase):
                                     content_type='application/json',
                                     headers={}
                                     )
-        self.assertEqual(response.json,
-                         "Registration should only contain username and password")
+        self.assertEqual(response.json["error"],
+                         "Invalid format. " +
+                         "Only username and Password are allowed")
+
+    def test_registration_with_no_data(self):
+        response = self.client.post('/auth/register',
+                                    data=json.dumps({}),
+                                    content_type='application/json',
+                                    headers={}
+                                    )
+        self.assertEqual(response.json["error"],
+                         "Please enter registration data")
+
+    def test_registration_with_missing_data(self):
+        data = json.dumps({"username": "Tester"})
+        response = self.client.post('/auth/register',
+                                    data=data,
+                                    content_type='application/json',
+                                    headers={}
+                                    )
+        self.assertEqual(response.json["error"],
+                         "username or password is missing")
 
     def test_registration_with_existing_username(self):
-
-        self.client.post('/auth/register',
-                         data=self.credentials,
-                         content_type='application/json',
-                         headers={}
-                         )
+        data = json.dumps({"username": "Tester",
+                           "password": "password"})
         response = self.client.post('/auth/register',
-                                    data=self.credentials,
+                                    data=data,
                                     content_type='application/json',
                                     headers={}
                                     )
-        self.assertEqual(response.json, "User already exists")
+        self.assertEqual(response.json["message"], "Tester already exists")
 
     def test_registration_works(self):
-
+        data = json.dumps({"username": "Hugo",
+                           "password": 1234})
         response = self.client.post('/auth/register',
-                                    data=self.credentials,
+                                    data=data,
                                     content_type='application/json',
                                     headers={}
                                     )
-        self.assertEqual(response.json, "Registration successful")
+        self.assertEqual(response.json["message"],
+                         "Successfully registered Hugo")
 
-    def test_login_with_invalid_credentials(self):
-
-        login_data = json.dumps({"username": "Hugo",
-                                 "password": "invalid"
-                                 })
-        self.client.post('/auth/register',
-                         data=self.credentials,
-                         content_type='application/json',
-                         headers={}
-                         )
+    def test_login_with_invalid_password(self):
+        data = json.dumps({"username": "Tester",
+                           "password": "wrong"})
         response = self.client.post('/auth/login',
-                                    data=login_data,
+                                    data=data,
                                     content_type='application/json',
                                     headers={}
                                     )
-        self.assertEqual(response.json, "Invalid credentials")
+        self.assertEqual(response.json["error"], "Invalid password")
+
+    def test_login_with_missing_data(self):
+        data = json.dumps({"username": "Tester"})
+        response = self.client.post('/auth/login',
+                                    data=data,
+                                    content_type='application/json',
+                                    headers={}
+                                    )
+        self.assertEqual(response.json["error"],
+                         "username or password is missing")
+
+    def test_login_with_no_data(self):
+        response = self.client.post('/auth/login',
+                                    content_type='application/json',
+                                    headers={}
+                                    )
+        self.assertEqual(response.json["error"],
+                         "Login data not found")
 
     def test_login_works(self):
-
-        registration_data = json.dumps({"username": "Hugo",
-                                        "password": 1234
-                                        })
-        login_data = json.dumps({"username": "Hugo",
-                                 "password": 1234
-                                 })
-        self.client.post('/auth/register',
-                         data=registration_data,
-                         content_type='application/json',
-                         headers={}
-                         )
+        data = json.dumps({"username": "Tester",
+                           "password": "password"
+                           })
         response = self.client.post('/auth/login',
-                                    data=login_data,
+                                    data=data,
                                     content_type='application/json',
                                     headers={}
                                     )
-        self.assertEqual(response.json, "Login successful")
-
-    def test_it_requires_token(self):
-        pass
-
-    def test_for_invalid_token(self):
-        pass
-
-    def test_for_expired_token(self):
-        pass
+        self.assertEqual(response.json["message"], "Login successful")
