6e894bed74e0c9f6c3187d427dbef48cf626e517
Boniface Mwenda <boniface.mwenda@andela.com>
2017-01-27 00:18:32 +0300

    Added test user and bucketlist in setUp method

--- app/tests/base_test_case.py
+++ app/tests/base_test_case.py
@@ -1,6 +1,11 @@
+from datetime import datetime, timedelta
+import jwt
+
 from flask_testing import TestCase
-from config import config_environments
-from manage import app
+
+from app.models import User, BucketList, BucketListItem
+from config import Config, config_environments
+from manage import app, db
 
 
 class BaseTestCase(TestCase):
@@ -10,7 +15,35 @@ class BaseTestCase(TestCase):
         return app
 
     def setUp(self):
-        self.client = self.create_app().test_client()
+        self.client = self.app.test_client()
+        db.drop_all()
+        db.create_all()
+
+        # Add a test user to test db
+        user = User("Tester", "password")
+        db.session.add(user)
+        db.session.commit()
+
+        # Generate token
+        payload = {"sub": user.id,
+                   "exp": datetime.utcnow() + timedelta(minutes=5)
+                   }
+        encoded_token = jwt.encode(
+            payload, Config.SECRET_KEY, algorithm='HS256')
+        self.token = encoded_token.decode('utf-8')
+
+        # Add a buckelist to test db
+        user = User.query.filter_by(username="Tester").first()
+        bucketlist = BucketList("Test bucketlist", user.id, user.username)
+        db.session.add(bucketlist)
+        db.session.commit()
+
+        # Add a bucketlist item to test db
+        bucketlist = BucketList.query.filter_by(name="Test bucketlist").first()
+        bucketlist_item = BucketListItem("Test item", bucketlist.id)
+        db.session.add(bucketlist_item)
+        db.session.commit()
 
     def tearDown(self):
-        pass
+        db.session.remove()
+        db.drop_all()
